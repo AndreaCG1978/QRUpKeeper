b6dd487b306f434327bca1f92e2284330a8e9472
AndreaCG1978 <44733992+AndreaCG1978@users.noreply.github.com>
2019-12-22 12:45:01 -0300

    Add files via upload

--- datacenterforms.php
+++ datacenterforms.php
@@ -1,4 +1,4 @@
- <?php
+<?php
     include "config.php";
     include "utils.php";
     $dbConn =  connect($db);
@@ -12,7 +12,7 @@
         {
 		  
 		  $sql = $dbConn->prepare("SELECT * FROM forms where nroForm = '".$_GET['nroForm']."'");
-          $sql->execute();
+                  $sql->execute();
 		  $sql->setFetchMode(PDO::FETCH_ASSOC);
 		  header("HTTP/1.1 200 OK");
 		  echo json_encode( $sql->fetchAll());
@@ -58,7 +58,55 @@
 			echo json_encode( $statement->execute());
 			exit();
 			
+    }
+//Actualizar
+    if ($_SERVER['REQUEST_METHOD'] == 'PUT')
+    {
+	//$input = $_GET;
+	echo "<script type='text/javascript'>
+                alert('ENTRA PUT!');
+            </script>";
+	parse_str(file_get_contents("php://input"), $_PUT);
+	foreach ($_PUT as $key => $value)
+	{
+	    unset($_PUT[$key]);
+	    $_PUT[str_replace('amp;', '', $key)] = $value;
 	}
+	$_REQUEST = array_merge($_REQUEST, $_PUT);
+	$input = $_PUT;
+	$itemId = $_PUT['id'] ;
+	$fields = getParams($input);
+	$sql = "UPDATE forms  SET $fields  WHERE id=$itemId ";
+	echo "<script type='text/javascript'>
+                alert('SQL ACTUALIZAR:".$sql."');
+            </script>";
+    	$statement = $dbConn->prepare($sql);
+	bindAllValues($statement, $input);
+	$result = $statement->execute();
+	echo "<script type='text/javascript'>
+                alert('RESULTADO:".$result."');
+            </script>";
+
+       
+	
+	
+	
+	/*
+	$sql = "UPDATE item SET (name=:name, description=:description) WHERE id=$itemId ";
+	
+	echo  "<script>SQL FINAL: ". $sql."</script>"; 
+    	$statement = $dbConn->prepare($sql);
+    //$statement->bindParam (":id",  $_PUT['id'] , PDO::PARAM_INT);
+	$statement->bindParam (":name",  $_PUT['name'] , PDO::PARAM_STR);
+    $statement->bindParam (":description",   $_PUT['description'] , PDO::PARAM_STR);
+	$statement->execute();*/
+	   
+	   
+	   
+	   
+      //  header("HTTP/1.1 200 OK");
+        exit();
+    }
     
     //En caso de que ninguna de las opciones anteriores se haya ejecutado
     header("HTTP/1.1 400 Bad Request");
--- tableros.php
+++ tableros.php
@@ -2,54 +2,67 @@
     include "config.php";
     include "utils.php";
     $dbConn =  connect($db);
-    echo "<script>EJECUTA ARCHIVO</script>"; 
     //En caso de que ninguna de las opciones anteriores se haya ejecutado
     if ($_SERVER['REQUEST_METHOD'] == 'GET')
     {
-        if (isset($_GET['id']))
+        if (isset($_GET['codigo']) && isset($_GET['name']) && isset($_GET['idForm']))
         {
           //Mostrar un registro
-          $sql = $dbConn->prepare("SELECT * FROM tablero_tgbt where id=:id");
-          $sql->bindValue(':id', $_GET['id']);
+	  $tipoTablero = $_GET['codigo'];
+	  if($tipoTablero =='101'){
+		$sqlSearch = "SELECT * FROM tablero_tgbt where name='".$_GET['name']."' and idForm =".$_GET['idForm']."";
+ 	  }
+	  if($tipoTablero =='102'){
+		$sqlSearch = "SELECT * FROM tablero_airechiller where name='".$_GET['name']."' and idForm =".$_GET['idForm']."";
+	  }
+	  if($tipoTablero =='103'){
+		$sqlSearch = "SELECT * FROM tablero_crac where name='".$_GET['name']."' and idForm =".$_GET['idForm']."";
+	  }
+	  if($tipoTablero =='104'){
+		$sqlSearch = "SELECT * FROM tablero_inups where name='".$_GET['name']."' and idForm =".$_GET['idForm']."";
+	  }
+          $sql = $dbConn->prepare($sqlSearch);
           $sql->execute();
+	  $sql->setFetchMode(PDO::FETCH_ASSOC);
           header("HTTP/1.1 200 OK");
-          echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
+          echo json_encode( $sql->fetchAll());
           exit();
-        }
-        else {
-          //Mostrar lista de registros
-          $sql = $dbConn->prepare("SELECT * FROM tablero_tgbt");
-          $sql->execute();
-          $sql->setFetchMode(PDO::FETCH_ASSOC);
-          header("HTTP/1.1 200 OK");
-          echo json_encode( $sql->fetchAll()  );
-          exit();
-      }
+
+        }else{
+	  //Mostrar lista de registros
+	$sql = $dbConn->prepare("SELECT * FROM tablero_tgbt");
+	$sql->execute();
+	$sql->setFetchMode(PDO::FETCH_ASSOC);
+	header("HTTP/1.1 200 OK");
+	echo json_encode( $sql->fetchAll());
+	exit();
+	}
+
     }
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    if ($_SERVER['REQUEST_METHOD'] === 'POST')
     {
-		echo "<script>ENTRA AL FUCKING POST</script>"; 
+		
 		$tipoTablero = $_POST['codigo'];
 		echo "<script> Tipo tablero: ".$tipoTablero."</script>"; 
 		if($tipoTablero =='101'){
-			$sql = "INSERT INTO tablero_tgbt(name, codigo,idForm, kwr, kws, kwt, par, pas, pat)
+			$sqlInsert = "INSERT INTO tablero_tgbt(name, codigo,idForm, kwr, kws, kwt, par, pas, pat)
 	              VALUES(:name, :codigo, :idForm, :kwr, :kws, :kwt, :par, :pas, :pat)";
 		}
 		if($tipoTablero =='102'){
-			$sql = "INSERT INTO tablero_airechiller(name, codigo,idForm, kwr, kws, kwt, par, pas, pat)
+			$sqlInsert = "INSERT INTO tablero_airechiller(name, codigo,idForm, kwr, kws, kwt, par, pas, pat)
 	              VALUES(:name, :codigo, :idForm, :kwr, :kws, :kwt, :par, :pas, :pat)";
 		}
 		if($tipoTablero =='103'){
-			$sql = "INSERT INTO tablero_crac(name, codigo,idForm, kwr, kws, kwt, par, pas, pat)
+			$sqlInsert = "INSERT INTO tablero_crac(name, codigo,idForm, kwr, kws, kwt, par, pas, pat)
 	              VALUES(:name, :codigo, :idForm, :kwr, :kws, :kwt, :par, :pas, :pat)";
 		}
 		if($tipoTablero =='104'){
-			$sql = "INSERT INTO tablero_inups(name, codigo,idForm, kwr, kws, kwt, par, pas, pat)
+			$sqlInsert = "INSERT INTO tablero_inups(name, codigo,idForm, kwr, kws, kwt, par, pas, pat)
 	              VALUES(:name, :codigo, :idForm, :kwr, :kws, :kwt, :par, :pas, :pat)";
 		}
-		echo "<script> SQL: ".$sql."</script>"; 
-    		$statement = $dbConn->prepare($sql);
+		echo "<script> SQL: ".$sqlInsert."</script>"; 
+    		$statement = $dbConn->prepare($sqlInsert);
 		$statement->bindParam (":name", $_POST['name'] , PDO::PARAM_STR);
 		$statement->bindParam (":codigo", $_POST['codigo'] , PDO::PARAM_STR);
     		$statement->bindParam (":idForm",  $_POST['idForm'] , PDO::PARAM_STR);
@@ -60,11 +73,17 @@
 		$statement->bindParam (":pas",  $_POST['pas'] , PDO::PARAM_STR);
 		$statement->bindParam (":pat",  $_POST['pat'] , PDO::PARAM_STR);
 		$statement->execute();
+	/*	$lastId = $dbConn->lastInsertId();
+		echo "<script> SQL: ".$sqlSearch."</script>"; 
+		//realizo la busqueda del ultimo insertado
+		$sql = $dbConn->prepare($sqlSearch);
+	        $sql->bindValue(':id', $lastId);
+	        $sql->execute();
+		$sql->setFetchMode(PDO::FETCH_ASSOC);
+         	header("HTTP/1.1 200 OK");
+          	echo json_encode( $sql->fetchAll());*/
 		exit();			
 			
 	}
-	
-	
-    echo "<script>LLEGA FIN</script>"; 
     header("HTTP/1.1 400 Bad Request");
     ?>
