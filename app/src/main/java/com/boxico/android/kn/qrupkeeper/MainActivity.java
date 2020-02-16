69b8d49f2b339cb6647462a11ba3624b393ea2bb
AndreaCG1978 <grassanoandrea@gmail.com>
2020-02-16 10:26:35 -0300

    feb 16

--- app/src/main/java/com/boxico/android/kn/qrupkeeper/MainActivity.java
+++ app/src/main/java/com/boxico/android/kn/qrupkeeper/MainActivity.java
@@ -4092,7 +4092,7 @@ public class MainActivity extends ExpandableListFragment implements ZXingScanner
         setContentView(mScannerView);  // It's opensorce api, so it work only with setContentView(...)
         mScannerView.setResultHandler(this);
         mScannerView.startCamera();*/
-        idQr = 109;
+        idQr = 115;
         selectedArtefact = null;
         this.openEntrySpecifyForm();
     }
